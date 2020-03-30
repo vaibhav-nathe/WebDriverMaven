4b8b1ac74cf92e4c2545013ddfb14efbd45903c9
unknown <vaibhav.nathe@gmail.com>
2020-03-30 11:19:44 +0530

    Initial commit

---
+++ src/test/java/TestApp.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestApp {
+
+	public static WebDriver driver;
+		@Test
+			public void test() {
+			Assert.assertEquals("", "");
+			DesiredCapabilities cap = DesiredCapabilities.internetExplorer();
+			cap.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+			cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+			cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			//System.setProperty("webdriver.ie.driver","C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\IEDriverServer.exe");
+			System.setProperty("webdriver.ie.driver","D:\\Selenium jars\\chromedriver_win32\\IEDriverServer.exe");
+			 WebDriver driver = new InternetExplorerDriver(cap);
+			driver.get("https://google.com");
+				
+			
+		}
+
+		}
+
