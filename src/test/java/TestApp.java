e37b6819fb8096ada33a12d1f3aa7062c6ab07f7
unknown <vaibhav.nathe@gmail.com>
2020-04-10 23:40:48 +0530

    last change

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,20 +10,21 @@ public class TestApp {
 
 	public static WebDriver driver;
 		@Test
-			public void test() {
+			public void test() throws InterruptedException {
 			Assert.assertTrue(true);
-			DesiredCapabilities cap = DesiredCapabilities.internetExplorer();
-			cap.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
-			cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+			DesiredCapabilities cap = DesiredCapabilities.chrome();
+			//DesiredCapabilities cap = DesiredCapabilities.internetExplorer();
+			//cap.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+			//cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
 			cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 			//System.setProperty("webdriver.ie.driver","C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\IEDriverServer.exe");
-			System.setProperty("webdriver.ie.driver","D:\\Selenium jars\\chromedriver_win32\\IEDriverServer.exe");
-			 WebDriver driver = new InternetExplorerDriver(cap);
+			//System.setProperty("webdriver..driver","D:\\Selenium jars\\chromedriver_win32\\IEDriverServer.exe");
+			System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
+			 WebDriver driver = new ChromeDriver(cap);
 			driver.get("https://google.com");
-				System.out.println("code executed successfully");
-			//git remote add originto https://github.com/vaibhav-nathe/WebDriverMaven.git
+			
+			System.out.println("Code executed successfully");
+			Thread.sleep(3000);
 			
 		}
-
-		}
-
+}
