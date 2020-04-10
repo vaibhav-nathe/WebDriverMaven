8c16f0f7c3776283193ec17090d554a2cfdff1ea
vaibhav-nathe <62811541+vaibhav-nathe@users.noreply.github.com>
2020-04-10 22:14:00 +0530

    Changed the DC to chrome

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -12,9 +12,10 @@ public class TestApp {
 		@Test
 			public void test() {
 			Assert.assertEquals("", "");
+			DesiredCapabilities cap = DesiredCapabilities.chrome();
 			DesiredCapabilities cap = DesiredCapabilities.internetExplorer();
-			cap.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
-			cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+			//cap.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+			//cap.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
 			cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 			//System.setProperty("webdriver.ie.driver","C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\IEDriverServer.exe");
 			//System.setProperty("webdriver..driver","D:\\Selenium jars\\chromedriver_win32\\IEDriverServer.exe");
